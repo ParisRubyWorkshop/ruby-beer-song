420b8ae1cae9ac6d633a0cdfa7ed2526532955bc
Sandra <sandstudio@gmail.com>
2016-10-12 21:11:43 +0200

    Refactor pluralize

--- beer_song.rb
+++ beer_song.rb
@@ -1,17 +1,24 @@
-class BeerSong
-  def initialize
+class String
+  def pluralize(count)
+    string = self.to_s
+    word = (count > 1 || count == 0) ? "#{string}s" : string
+    count = "no more" if count == 0
+    "#{count} #{word}"
   end
+end
+
+class BeerSong
 
   def verse(line_number)
     one_word        = line_number == 1 ? "it" : "one"
-    first_word      = pluralize("bottle", line_number)
-    second_sentence = "Take #{one_word} down and pass it around, #{pluralize("bottle", line_number - 1)} of beer on the wall.\n"
+    first_word      = "bottle".pluralize(line_number)
+    second_sentence = "Take #{one_word} down and pass it around, #{"bottle".pluralize(line_number - 1)} of beer on the wall.\n"
     if line_number == 0
       first_word = first_word.capitalize
       second_sentence = "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     end
 
-    "#{first_word} of beer on the wall, #{pluralize("bottle", line_number)} of beer.\n" \
+    "#{first_word} of beer on the wall, #{"bottle".pluralize(line_number)} of beer.\n" \
     "#{second_sentence}"
   end
 
@@ -19,12 +26,6 @@ class BeerSong
     (b..a).map do |index|
       verse(index)
     end.reverse.join("\n")
-  end
-
-  def pluralize(string, count)
-    word = (count > 1 || count == 0) ? "#{string}s" : string
-    count = "no more" if count == 0
-    "#{count} #{word}"
   end
 
   def lyrics
