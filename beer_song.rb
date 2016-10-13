5561753db2b16e8ed9b000fd839e270a240155f5
Nicolas ZERMATI <nicoolas25@gmail.com>
2016-10-13 12:10:14 +0200

    Avoid some duplication using data structures

--- beer_song.rb
+++ beer_song.rb
@@ -1,4 +1,5 @@
 class BeerSong
+
   def lyrics
     verses(99, 0)
   end
@@ -10,19 +11,59 @@ class BeerSong
   end
 
   def verse(number)
-    case number
-    when 0
-      "No more bottles of beer on the wall, no more bottles of beer.\n" \
-      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
-    when 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\n" \
-      "Take it down and pass it around, no more bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\n" \
-      "Take one down and pass it around, 1 bottle of beer on the wall.\n"
-    else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" \
-      "Take one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
-    end
+    verse = Verse.new(number)
+    LINES.map { |line| line % verse }.map(&:capitalize).join
+  end
+
+  private
+
+  LINES = [
+    "%{bottle} of beer on the wall, %{bottle} of beer.\n".freeze,
+    "%{action}, %{bottle_left} of beer on the wall.\n".freeze,
+  ].freeze
+
+end
+
+Verse = Struct.new(:number) do
+  def to_hash
+    @hash ||=
+      case number
+      when 0 then defaults.merge(bottle_0)
+      when 1 then defaults.merge(bottle_1)
+      when 2 then defaults.merge(bottle_2)
+      else defaults
+      end
+  end
+
+  private
+
+  def defaults
+    {
+      bottle: "#{number} bottles",
+      bottle_left: "#{number - 1} bottles",
+      action: "Take one down and pass it around",
+    }
+  end
+
+  def bottle_0
+    {
+      bottle: "no more bottles",
+      bottle_left: "99 bottles",
+      action: "Go to the store and buy some more",
+    }
+  end
+
+  def bottle_1
+    {
+      bottle: "1 bottle",
+      bottle_left: "no more bottles",
+      action: "Take it down and pass it around",
+    }
+  end
+
+  def bottle_2
+    {
+      bottle_left: "1 bottle",
+    }
   end
 end
